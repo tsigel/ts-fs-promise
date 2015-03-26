6cb34886120fff08129e2084b442cb11feb9b8ce
daniil <tsigelnitskiy@narr8.me>
2015-03-26 17:41:20 +0300

    up version add API

--- src/FsPromise.ts
+++ src/FsPromise.ts
@@ -9,54 +9,151 @@ var Promise = modulePromise.Promise;
 
 class FsPromise implements tsFsPromise.Main {
 
-    public readJSON(path:string):Promise<any> {
-        return this.getFsPromise("readJSON", [path]);
+    public copy(src:string, dest:string, filter?:(src:string) => boolean):Promise<boolean> {
+        return this.getFsPromise("copy", [src, dest, filter]);
     }
 
-    public readFile(path:string, encoding:string):Promise<any> {
-        return this.getFsPromise("readFile", [path , encoding]);
-    }
-
-    public copy(src:string, dest:string):Promise<boolean> {
-        return this.getFsPromise("copy", [src, dest]);
+    public copySync(src:string, dest:string, filter?:(src:string) => boolean):void {
+        fs.copySync(src, dest, filter);
     }
 
     public createFile(file:string):Promise<boolean> {
         return this.getFsPromise("createFile", [])
     }
 
+    public createFileSync(file:string):void {
+        fs.createFileSync(file);
+    }
+
     public mkdirs(dir:string):Promise<boolean> {
         return this.getFsPromise("mkdirs", [dir]);
     }
+
     public mkdirp(dir:string):Promise<boolean> {
         return this.getFsPromise("mkdirp", [dir]);
     }
 
-    public outputFile(file: string, data: any):Promise<boolean> {
+    public mkdirsSync(dir:string):void {
+        fs.mkdirsSync(dir);
+    }
+
+    public mkdirpSync(dir:string):void {
+        fs.mkdirpSync(dir);
+    }
+
+    public outputFile(file:string, data:any):Promise<boolean> {
         return this.getFsPromise("outputFile", [file, data]);
     }
 
-    public outputJSON(file: string, data: any):Promise<boolean> {
+    public outputFileSync(file:string, data:any):void {
+        fs.outputFileSync(file, data);
+    }
+
+    public outputJSON(file:string, data:any):Promise<boolean> {
         return this.getFsPromise("outputJSON", [file, data]);
     }
 
-    public remove(dir: string):Promise<boolean> {
+    public outputJSONSync(file:string, data:any):void {
+        fs.outputJSONSync(file, data);
+    }
+
+    public readJSON(file:string, options?:tsFsPromise.OpenOptions):Promise<any> {
+        return this.getFsPromise("readJSON", [file, options]);
+    }
+
+    public readJSONSync(file:string, options?:tsFsPromise.OpenOptions):any {
+        return fs.readJSONSync(file, options);
+    }
+
+    public remove(dir:string):Promise<boolean> {
         return this.getFsPromise("remove", [dir]);
     }
 
-    public writeJSON(file: string, object: any, options?: tsFsPromise.OpenOptions):Promise<boolean> {
+    public removeSync(dir:string):void {
+        fs.removeSync(dir);
+    }
+
+    public writeJSON(file:string, object:any, options?:tsFsPromise.OpenOptions):Promise<boolean> {
         return this.getFsPromise("writeJSON", [file, object, options]);
     }
 
-    public rename(oldPath: string, newPath: string):Promise<boolean> {
+    public writeJSONSync(file:string, object:any, options?:tsFsPromise.OpenOptions):void {
+        fs.writeJSONSync(file, object, options);
+    }
+
+    public rename(oldPath:string, newPath:string):Promise<boolean> {
         return this.getFsPromise("rename", [oldPath, newPath]);
     }
 
-    public stat(path: string):Promise<tsFsPromise.Stats> {
+    public renameSync(oldPath:string, newPath:string):void {
+        fs.renameSync(oldPath, newPath);
+    }
+
+    public truncate(fd:number, len:number):Promise<boolean> {
+        return this.getFsPromise("truncate", [fd, len]);
+    }
+
+    public truncateSync(fd:number, len:number):void {
+        fs.truncateSync(fd, len);
+    }
+
+    public chown(path:string, uid:number, gid:number):Promise<boolean> {
+        return this.getFsPromise("chown", [path, uid, gid]);
+    }
+
+    public chownSync(path:string, uid:number, gid:number):void {
+        fs.chownSync(path, uid, gid);
+    }
+
+    public fchown(fd:number, uid:number, gid:number):Promise<boolean> {
+        return this.getFsPromise("fchown", [fd, uid, gid]);
+    }
+
+    public fchownSync(fd:number, uid:number, gid:number):void {
+        fs.fchownSync(fd, uid, gid);
+    }
+
+    public lchown(path:string, uid:number, gid:number):Promise<boolean> {
+        return this.getFsPromise("lchown", [path, uid, gid]);
+    }
+
+    public lchownSync(path:string, uid:number, gid:number):void {
+        fs.lchownSync(path, uid, gid);
+    }
+
+    public chmodSync(path:string, mode:number):void {
+        fs.chmodSync(path, mode);
+    }
+
+    public fchmod(fd:number, mode:number):Promise<boolean> {
+        return this.getFsPromise("fchmod", [fd, mode])
+    }
+
+    public fchmodSync(fd:number, mode:number):void {
+        fs.fchmodSync(fd, mode);
+    }
+
+    public lchmod(path:string, mode:string):Promise<boolean> {
+        return this.getFsPromise("lchmod", [path, mode]);
+    }
+
+    public lchmodSync(path:string, mode:string):void {
+        fs.lchmodSync(path, mode);
+    }
+
+    public stat(path:string):Promise<tsFsPromise.Stats> {
         return this.getFsPromise("stat", [path]);
     }
 
-    public writeFile(filename: string, data: any, encoding?: string):Promise<boolean> {
+    public statSync(path: string): tsFsPromise.Stats {
+        return fs.statSync(path);
+    }
+
+    public readFile(path:string, encoding:string):Promise<any> {
+        return this.getFsPromise("readFile", [path, encoding]);
+    }
+
+    public writeFile(filename:string, data:any, encoding?:string):Promise<boolean> {
         return this.getFsPromise("writeFile", [filename, data, encoding]);
     }
 
--- types/FsPromise.d.ts
+++ types/FsPromise.d.ts
@@ -29,20 +29,44 @@ declare module tsFsPromise {
     }
 
     interface Main {
-        readJSON(path:string):Promise<any>;
-        readFile(path:string, encoding:string):Promise<any>;
-        copy(src:string, dest:string):Promise<boolean>;
+        copy(src:string, dest:string, filter?:(src:string) => boolean):Promise<boolean>;
+        copySync(src:string, dest:string, filter?:(src:string) => boolean):void;
         createFile(file:string):Promise<boolean>;
+        createFileSync(file:string):void;
         mkdirs(dir:string):Promise<boolean>;
         mkdirp(dir:string):Promise<boolean>;
-        outputFile(file: string, data: any):Promise<boolean>;
-        outputJSON(file: string, data: any):Promise<boolean>;
-        remove(dir: string):Promise<boolean>;
-        writeJSON(file: string, object: any, options?: tsFsPromise.OpenOptions):Promise<boolean>;
-        rename(oldPath: string, newPath: string):Promise<boolean>;
-        stat(path: string):Promise<tsFsPromise.Stats>;
-        writeFile(filename: string, data: any, encoding?: string):Promise<boolean>;
-        readdir(path:string):Promise<Array<string>>
+        mkdirsSync(dir:string):void;
+        mkdirpSync(dir:string):void;
+        outputFile(file:string, data:any):Promise<boolean>;
+        outputFileSync(file:string, data:any):void;
+        outputJSON(file:string, data:any):Promise<boolean>;
+        outputJSONSync(file:string, data:any):void;
+        readJSON(file:string, options?:tsFsPromise.OpenOptions):Promise<any>;
+        readJSONSync(file:string, options?:tsFsPromise.OpenOptions):any;
+        remove(dir:string):Promise<boolean>;
+        removeSync(dir:string):void;
+        writeJSON(file:string, object:any, options?:tsFsPromise.OpenOptions):Promise<boolean>;
+        writeJSONSync(file:string, object:any, options?:tsFsPromise.OpenOptions):void;
+        rename(oldPath:string, newPath:string):Promise<boolean>;
+        renameSync(oldPath:string, newPath:string):void;
+        truncate(fd:number, len:number):Promise<boolean>;
+        truncateSync(fd:number, len:number):void;
+        chown(path:string, uid:number, gid:number):Promise<boolean>;
+        chownSync(path:string, uid:number, gid:number):void;
+        fchown(fd:number, uid:number, gid:number):Promise<boolean>;
+        fchownSync(fd:number, uid:number, gid:number):void;
+        lchown(path:string, uid:number, gid:number):Promise<boolean>;
+        lchownSync(path:string, uid:number, gid:number):void;
+        chmodSync(path:string, mode:number):void;
+        fchmod(fd:number, mode:number):Promise<boolean>;
+        fchmodSync(fd:number, mode:number):void;
+        lchmod(path:string, mode:string):Promise<boolean>;
+        lchmodSync(path:string, mode:string):void;
+        stat(path:string):Promise<tsFsPromise.Stats>;
+        statSync(path: string): tsFsPromise.Stats;
+        readFile(path:string, encoding:string):Promise<any>;
+        writeFile(filename:string, data:any, encoding?:string):Promise<boolean>;
+        readdir(path:string):Promise<Array<string>>;
     }
 
 }
