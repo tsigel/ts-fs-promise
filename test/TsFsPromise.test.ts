86e1a6b2c7e2f0434868f5c94239a8ba50fad8cf
daniil <tsddaniil@gmail.com>
2015-09-14 14:35:40 +0300

    add tests

--- test/TsFsPromise.test.ts
+++ test/TsFsPromise.test.ts
@@ -8,6 +8,17 @@ var getPath = function (path) {
     return ('./test/' + path.replace('./', '')).replace(/\/\//g, '/');
 };
 
+var getTestFilePath = function () {
+    return getPath('test/test.text');
+};
+
+var check = function (promise:Promise<any>) {
+    promise.catch(() => {
+        expect().fail('File system error!');
+    });
+    return promise;
+};
+
 try {
     fs.mkdirSync(getPath('test'));
 } catch (e) {
@@ -24,7 +35,7 @@ describe('ts-fs-promise', () => {
     it('copySync', () => {
 
         var ok;
-        fsPromise.copySync(getPath('test/test.text'), getPath('test.text'));
+        fsPromise.copySync(getTestFilePath(), getPath('test.text'));
         ok = (fs.readFileSync(getPath('test.text'), 'utf8') == 'test');
         expect(ok).to.be(true);
         fs.unlinkSync(getPath('test.text'));
@@ -51,10 +62,20 @@ describe('ts-fs-promise', () => {
 
     });
 
+    it("readFileSync", () => {
+        expect(fsPromise.readFileSync(getTestFilePath(), "utf8")).to.be('test');
+    });
+
+    it("writeFileSync", () => {
+        fs.writeFileSync(getPath("test.txt"), "test");
+        expect(fs.readFileSync(getPath("test.txt"), "utf8")).to.be('test');
+        fs.unlinkSync(getPath("test.txt"));
+    });
+
     it('copy', (done) => {
 
         var ok = false;
-        fsPromise.copy(getPath('test/test.text'), getPath('test.text')).then((status:boolean) => {
+        check(fsPromise.copy(getPath('test/test.text'), getPath('test.text'))).then((status:boolean) => {
             ok = (status && fs.readFileSync(getPath('test.text'), 'utf8') == 'test');
             expect(ok).to.be(true);
             fs.unlinkSync(getPath('test.text'));
@@ -66,7 +87,7 @@ describe('ts-fs-promise', () => {
     it('createFile', (done) => {
 
         var ok = false;
-        fsPromise.createFile(getPath('test.text')).then((status:boolean) => {
+        check(fsPromise.createFile(getPath('test.text'))).then((status:boolean) => {
             ok = (status && fs.existsSync(getPath('test.text')));
             expect(ok).to.be(true);
             fs.unlinkSync(getPath('test.text'));
@@ -78,10 +99,32 @@ describe('ts-fs-promise', () => {
     it('mkdirs', (done) => {
 
         var ok = false;
-        fsPromise.mkdirs(getPath('some')).then((status:boolean) => {
+        check(fsPromise.mkdirs(getPath('some'))).then((status:boolean) => {
             ok = (status && fs.existsSync(getPath('some')));
             expect(ok).to.be(true);
             fs.rmdir(getPath('some'));
+            done();
+        });
+
+    });
+
+    it('readFile', (done) => {
+
+        var ok = false;
+        check(fsPromise.readFile(getPath('test/test.text'), 'utf8')).then((fileText:string) => {
+            ok = fileText == 'test';
+            expect(ok).to.be(true);
+            done();
+        });
+
+    });
+
+    it("writeFile", (done) => {
+
+        var ok = false;
+        check(fsPromise.writeFile(getPath('test.txt'), 'test')).then(() => {
+            expect(fs.readFileSync(getPath('test.txt'), 'utf8')).to.be('test');
+            fs.unlinkSync(getPath('test.txt'));
             done();
         });
 
